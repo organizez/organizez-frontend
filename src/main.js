53f787a2d1e4c567003d34fb4bb55395bf5943ee
alexandra.boangiu <alexandra.boangiu98@gmail.com>
2023-02-07 21:45:34 +0200

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faHatWizard)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
