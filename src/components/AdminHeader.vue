d031f1f1e23327cbfc57aaf0c87c42c521ce735c
alexandra.boangiu <alexandra.boangiu98@gmail.com>
2023-02-12 12:26:03 +0200

    add admin area

---
+++ src/components/AdminHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="admin-header-component">
+      <b-nav small>
+        <b-nav-item active></b-nav-item>
+        <b-nav-item>Link</b-nav-item>
+      </b-nav>
+    </div>
+</template>
+<script>
+// import axios from 'axios';
+// import $ from "jquery";
+ export default {
+    data() {
+      return {
+  
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style>
+
+</style>
