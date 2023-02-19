ca43e4b7ef7d9c1ae65bc34181ca09ec35fef143
baditoiudenisa <baditoiudenisa@gmail.com>
2023-02-19 20:21:16 +0200

    Implement homepage blog component

---
+++ src/components/HomepageBlog.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="homepage-card-blog-component">
+        <hr class="homepage-line">
+        <div>
+            <b-row class="row-homepage-blog-card">
+                <b-col class="col-homepage-blog-card" v-for = "(blog, index) in blogs" :key="index">
+                    <b-card class="homepage-blog-card"  :img-src="blog.image" img-alt="Image" img-height="200px" img-top :title="blog.nameArticle" >
+                        <b-card-text class="homepage-blog-card-text" >
+                              {{blog.shortDescription}}
+                        </b-card-text>
+                         <template class="template-footer-homepage-blog-card" #footer >
+                             <small class="text-muted">Data publicare articol: {{blog.dateArticle}}</small>
+                        </template>
+                    </b-card>
+                </b-col>
+            </b-row>
+        </div>
+        <hr class="homepage-line">
+    </div>
+    
+</template>
+<script>
+import axios from 'axios';
+// import $ from "jquery";
+ export default {
+    components: {
+    },
+    data() {
+      return {
+        blogs: []
+      }
+    },
+    methods: {
+        getBlogsByDate() {
+            this.blogs = [];
+            axios({
+                method: "get",
+                headers: {"accept":"application/json"},
+                url: "http://localhost:3000/blog/getBlogsByDate"
+            }).then(result => {
+                if(result.data.length > 0) {
+                    let blog = {
+                        image: "",
+                        nameArticle: "",
+                        shortDescription: "",
+                        dateArticle: ""
+                    }
+                    for( var i = 0; i < result.data.length; i++) {
+                        blog = {
+                            image: result.data[i].image,
+                            nameArticle: result.data[i].name_article,
+                            shortDescription: result.data[i].short_description,
+                            dateArticle: result.data[i].date_article.substring(0,10)
+                        }
+                        this.blogs.push(blog);
+                    }
+                    console.log(this.blogs)
+                }
+            })
+        },
+
+    },
+    computed: {
+    },
+    mounted() {
+        this.getBlogsByDate();
+    }
+  }
+</script>
+<style>
+.homepage-card-blog-component {
+    padding-bottom: 20px;
+    position: relative;
+    padding-left: 100px;
+    padding-right: 100px !important;
+    margin: 10px; 
+    }
+.homepage-blog-card {
+    font-family: 'Petrona';
+    font-weight: 300;
+    color: black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+.homepage-blog-card-text {
+    font-family: 'Petrona';
+    font-weight: 300;
+    color: black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 30px !important;
+}
+.text-muted {
+    font-family: 'Petrona';
+    font-weight: 300;
+    color: black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+}
+.row-homepage-blog-card {
+    display: flex !important;
+}
+.col-homepage-blog-card {
+    display: flex !important;
+    /* background-image: url('https://i.pinimg.com/236x/72/1b/24/721b24010451bb0c0636991d4f7d1044.jpg')!important;  */
+    background-image: url('https://i.pinimg.com/236x/ca/8e/c3/ca8ec3b3a1ab839332d391f7ec8ec897.jpg')!important; 
+    background-repeat: repeat !important;
+    background-size: 250px !important;
+    background-position: center !important;
+    padding-left: 100px !important;
+    /* background-color: #f7f7f2 !important; */
+    padding-bottom: 10px !important;
+    padding-top: 10px !important;
+
+}
+.homepage-line {
+    background-color: #ffd60a !important;
+    display: flex !important;
+    align-items: center !important;
+    justify-content: center !important;
+    padding-right: 100px;
+    color: #ffc300 !important;
+    height: 2px;
+    border: 0;
+    box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);
+}
+
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,27 @@
         </template>
       </vue-horizontal-list>
       </b-row>
+      <b-row class="about-us-row-home">
+        <div class="module-border-wrap">
+          <div class="about-us-homepage-class">
+            <div class="main-about-us-hompeage-class">
+              <b-card class="card-about-us-homepage-class"
+                img-src="https://i.pinimg.com/236x/a3/a8/2d/a3a82d408b862d026717a6239fa91cb3.jpg" img-alt="Card image" img-right >
+                <h4 class="h4-about-us-homepage-class">Despre noi</h4>
+                <h5 class="h5-class-space">________</h5>
+                <b-card-text>
+                  <p class="text-about-us-homepga-class">Momentele unice ale vieții sunt cele mai importante.</p>
+                  <p class="text-about-us-homepga-class">Iubim să aducem un zâmbet pe chipul oamenilor și să luăm parte la momente deosebite.</p>
+                  <p class="text-about-us-homepga-class">Acesta este și scopul nostru principal. Ne dorim să ne punem amprenta asupra evenimentului tau, </p>
+                  <p class="text-about-us-homepga-class">să surprindem cele mai fabuloase clipe care să îți încânte chipul de fiecare dată când arunci o privire. </p>
+                  <p class="text-about-us-homepga-class">Noi suntem o echipă de profeșioniști, pregătită să organizeze momentul tau!</p>
+                </b-card-text>
+              </b-card>
+            </div>
+          </div>
+        </div>
+      </b-row>
+      <HomePageBlog></HomePageBlog>
       <Footer></Footer>
     </div>
 </template>
@@ -19,12 +40,14 @@ import axios from 'axios';
 // import $ from "jquery";
 import MainHeader from "../components/MainHeader.vue";
 import Footer from "../components/Footer.vue";
+import HomePageBlog from "../components/HomepageBlog.vue"
 import VueHorizontalList from "vue-horizontal-list";
 
  export default {
     components: {
       MainHeader,
       Footer,
+      HomePageBlog,
       VueHorizontalList
     },
     data() {
@@ -81,4 +104,41 @@ import VueHorizontalList from "vue-horizontal-list";
     padding: 0 !important;
   }
 
+  .h5-class-space {
+    padding-bottom: 50px;
+    padding-left: 460px !important;
+  }
+  .text-about-us-homepga-class {
+    font-family: 'Petrona';
+    font-weight: 300;
+    color: black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    padding-left: 460px;
+  }
+  .h4-about-us-homepage-class {
+    font-family: 'Petrona';
+    font-weight: 300;
+    color: black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    padding-left: 460px;
+    
+  }
+  .card-about-us-homepage-class {
+    padding-right: 208px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    margin: 0 !important;
+    background-color: #cad2c5 !important ;
+  }
+  .h5-class-space{
+    padding-left: 190px;
+  }
+  .main-about-us-hompeage-class {
+    padding-top: 30px !important;
+    padding-bottom: 30px !important;
+    margin: 0px !important;
+  
+  }
 </style>
